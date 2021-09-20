dbf9303bfbbc4901fc2cbc72de70ccd527d3c43d
Dabin <dorothy9795@gmail.com>
2021-09-20 12:25:54 +0900

    예제 씬 추가

---
+++ ObscuredType/Assets/Resources/Scripts/Example.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    public Obscured<int> Score = new Obscured<int>(20);
+    public Obscured<string> Name = new Obscured<string>("ABC");
+
+    void Start()
+    {
+        Debug.Log($"Score : {Score.Value}");
+        Score.Value = 32;
+        Debug.Log($"Score : {Score.Value}");
+        Score.Hack_SetValue(52);
+        Debug.Log($"Score : {Score.Value}");
+
+        Debug.Log($"Name : {Name.Value}");
+        Name.Value = "CDE";
+        Debug.Log($"Name : {Name.Value}");
+        Name.Hack_SetValue("FGH");
+        Debug.Log($"Name : {Name.Value}");
+    }
+}
