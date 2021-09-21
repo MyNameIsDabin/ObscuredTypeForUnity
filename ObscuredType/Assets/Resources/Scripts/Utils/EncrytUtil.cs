4e0804201f2a0b4140bc6d6d55bef6fc0852b18a
Dabin <dorothy9795@gmail.com>
2021-09-21 16:58:41 +0900

    Update EncrytUtil.cs

--- ObscuredType/Assets/Resources/Scripts/Utils/EncrytUtil.cs
+++ ObscuredType/Assets/Resources/Scripts/Utils/EncrytUtil.cs
@@ -10,7 +10,20 @@ public class EncrytUtil
             var bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
 
             var stringBuilder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+                stringBuilder.Append(bytes[i].ToString("x2"));
 
+            return stringBuilder.ToString();
+        }
+    }
+
+    public static string GetMD5Hash(string rawData)
+    {
+        using (MD5 md5Hash = MD5.Create())
+        {
+            var bytes = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+            var stringBuilder = new StringBuilder();
             for (int i = 0; i < bytes.Length; i++)
                 stringBuilder.Append(bytes[i].ToString("x2"));
 
